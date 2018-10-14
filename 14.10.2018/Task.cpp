d5558cf5eed9bfd99f02dd3fce2e86b995564209
GrigoriiKond <44100293+GrigoriiKond@users.noreply.github.com>
2018-10-14 23:42:49 +0300

    Create Task.cpp

---
+++ 14.10.2018/Task.cpp
@@ -0,0 +1,32 @@
+/*Напишите программу находящую в массиве сумму элементов делящихся либо на 72 либо на 27(не вместе).*/
+
+#include <iostream>
+#include <numeric>
+
+using namespace std;
+
+
+int main() {
+
+unsigned int N,i;
+
+int sum,a;
+
+cin>>N;
+
+sum=0;
+
+for(i=0; i<N; i++) {
+
+	 cin>>a;
+
+   if ((a % 72 == 0)^(a % 27 == 0) )
+
+   sum+=a;
+
+  }
+
+cout<<sum;
+return 0;
+
+}
