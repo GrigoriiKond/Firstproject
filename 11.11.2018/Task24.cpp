84ec35a6b5e27c960c85ee1445a79dc68a678bde
GrigoriiKond <44100293+GrigoriiKond@users.noreply.github.com>
2018-11-11 23:41:10 +0300

    Update Task24.cpp

--- 11.11.2018/Task24.cpp
+++ 11.11.2018/Task24.cpp
@@ -60,10 +60,130 @@ struct Date
 			else {return 30;}
 		}
 	}
+	
+	   int me ()  {
+        if ((Month == 1) || (Month == 10)) {return 0;};
+        if (Month == 5) {return 1;};
+        if (Month == 8) {return 2;};
+        if ((Month == 2) || (Month == 3) || (Month == 11)) {return 3;};
+        if (Month == 6) {return 4;};
+        if ((Month == 9) || (Month == 12)) {return 5;};
+        if ((Month == 4) || (Month == 7)) {return 6;};
+    }
+
+    int ye ()  {
+        int n = (Year / 100);
+        n -= mdn(n, 4);
+        n *= 5;
+        n -= mdn(n, 7);
+        return n;
+    }
+
+
+    int ned ()  {
+        int m = Day + cod_mes();
+        m -= mdn(m, 7);
+
+        int p = (Year % 100);
+        p -= mdn(p, 28);
+        p += ((Year % 100)/4);
+        p += cod_year();
+        if (((Month == 1) || (Month == 2)) && (visokos() == 1)) {p--;};
+
+        m += p;
+        m -= mdn(m, 7);
+
+        return m;
+    }
+
+    int kon() {
+        int a = Day, b = Month, c = Year;
+        int p = 0;
+        while (Month < 12)
+        {
+            p += day();
+            Month++;
+        }
+        while (Day <= 31)
+        {
+            p++;
+            Day++;
+        }
+
+        Day = a;
+        Month = b;
+        Year = c;
+
+        return p;
+    } 
+
+    int &operator - (Date &B)   {
+        int a = Day, b = Month, c = Year;
+        int n = (kon() - kon()), p = 0;
+
+        if (Year > B.Year)
+        {
+            while (B.Year < Year)
+            {
+                B.Year++;
+                if (B.vis()) {p -= 366;}
+                else {p -= 365;};
+            }
+        }
+        else
+        {
+            while (Year < B.Year)
+            {
+                Year++;
+                if (vis()) {p += 366;}
+                else {p += 365;};
+            }
+        }
+
+        B.Day = 0;
+        B.Day += n;
+        B.Day += p;
+        if (B.Day < 0) {B.Day = (-B.Day);};
+
+        Day = a;
+        Month = b;
+        Year = c;
+
+        return (B.Day);
+    }
+
+    Date &operator + (int a)    {
+        Day += a;
+
+        while (Day > dni())
+        {
+            Day -= dni();
+            Month++;
+        }
+
+        while (Month > 12)
+        {
+            Month -= 12;
+            Year++;
+        }
+        return *this;
+    }
+
 
 };
 
 int main()
 {
+    Date D;
+    cout << "1-st date: ";
+    D.in();
+	
+    int p;
+    cout << "Number of days: ";
+    cin >> p;
+    D + p;
+    cout << "2-nd date: ";
+    D.out();
+    cout << '\n';
   return 0;
 }
