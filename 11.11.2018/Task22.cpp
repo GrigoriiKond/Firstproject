aeedaf2866b2230040cc3943e5474a2766ebc0bc
GrigoriiKond <44100293+GrigoriiKond@users.noreply.github.com>
2018-11-11 22:15:11 +0300

    Create Task22.cpp

---
+++ 11.11.2018/Task22.cpp
@@ -0,0 +1,92 @@
+/* Для структуры Date напишите метод, определяющий, сколько дней осталось до Нового Года. */
+
+#include <iostream>
+using namespace std;
+
+struct Date
+{
+	int Day;
+	int Month;
+	int Year;
+
+	Date ()
+	{
+		Day = 0;
+		Month = 0;
+		Year = 0;
+	}
+
+	void in()
+	{
+		cin >> Day >> Month >> Year;
+	}
+
+	void out()
+	{
+		if ((Day / 10) == 0) cout << 0;
+		cout << Day << ".";
+		if ((Month / 10) == 0) cout << 0;
+		cout << Month << "." << Year;
+	}
+
+	bool vis()
+	{
+		if ((Year % 400) == 0) {return true;}
+		else
+			{
+				if ((Year % 100) == 0) {return false;}
+				else
+				{
+					if ((Year % 4) == 0) {return true;}
+					else {return false;}
+				}
+			}
+	}
+
+	int day()
+	{
+		if (Month == 2)
+		{
+			if (vis()) {return 29;}
+			else {return 28;};
+		}
+		else
+		{
+			if (((Month <= 7) && (((Month % 2) != 0))) || ((Month >= 8) && ((Month % 2) == 0)))
+			{
+				return 31;
+			}
+			else {return 30;}
+		}
+	}
+
+	int kon()
+	{
+		int i = 0;
+		while (Month < 12)
+		{
+			i += day();
+			Month++;
+		}
+		while (Day <= 31)
+		{
+			i++;
+			Day++;
+		}
+
+		return i;
+	}
+};
+
+int main()
+{
+	Date Sa;
+
+	Sa.in();
+	Sa.out();
+	cout << '\n';
+
+	cout << Sa.kon();
+  
+  return 0;
+}
