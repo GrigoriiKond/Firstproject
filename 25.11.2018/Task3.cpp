b3740a3b1a279f028814a8aaea88e817d999cf95
GrigoriiKond <44100293+GrigoriiKond@users.noreply.github.com>
2018-11-25 22:12:54 +0300

    Update Task3.cpp

--- 25.11.2018/Task3.cpp
+++ 25.11.2018/Task3.cpp
@@ -1,1 +1,19 @@
+/* Напишите переборную программу программу, вычисляющую значение функции Эйлера для числа n. 
+Программа должна работать не более чем за O(n* log n) шагов. */
 
+#include <iostream>
+using namespace std;
+
+int main () {
+    long long n, a;
+    cin >> n;
+    a = n;
+    for (long long i = 2; i*i <= n; i++)
+        if (n % i == 0) {
+            while (n % i == 0) n/= i;
+            a-=a / i;
+        }
+    if (n > 1) a -=a/n;
+    cout << a;
+    return 0;   
+}
