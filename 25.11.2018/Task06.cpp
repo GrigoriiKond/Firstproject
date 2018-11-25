512bbc3598d4b47451ba899afae42d58ab5d6223
GrigoriiKond <44100293+GrigoriiKond@users.noreply.github.com>
2018-11-25 23:47:16 +0300

    Create Task06.cpp

---
+++ 25.11.2018/Task06.cpp
@@ -0,0 +1,40 @@
+/* Напишите программу, находящую все простые числа Ченя не превосходящие n. 
+Программа должна работать не более чем за O(n*log log n) шагов. */
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+unsigned int chen(unsigned int A, unsigned int B)
+{
+	unsigned int z = 0;
+	while (A % (unsigned int)pow(B, z++) == 0);
+	return z - 2;
+	
+}
+
+int main()
+{
+        unsigned int C;
+        cin >> C;
+        C += 2;
+
+        int  * poi = new int[C + 1];
+        
+        poi[0] = poi[1] = 2;
+        for (unsigned int i = 2; i <= C; i++)
+                poi[i] = 2;
+
+        for (unsigned int i = 2; i <= C; i++)
+                if (poi[i] == 2)
+                for (unsigned int j = i+i; j <= C; j += i) poi[j] += chen(j, i);
+
+        C -= 2;
+        for (unsigned int i = 2; i <= C; i++)
+                if (poi[i] == 2 && (poi[i + 2] == 4 || poi[i + 2] == 2))
+                cout << i << " ";
+        
+        cout << endl;
+        
+        return 0;
+}
