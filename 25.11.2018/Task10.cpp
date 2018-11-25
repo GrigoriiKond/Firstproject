3df96950e0a2ee3b362962d2a18b22c99b696abe
GrigoriiKond <44100293+GrigoriiKond@users.noreply.github.com>
2018-11-25 21:39:28 +0300

    Update and rename Task2.cpp to Task10.cpp

---
+++ 25.11.2018/Task10.cpp
@@ -0,0 +1,51 @@
+/* Напишите функцию вычисляющую праймориал числа. Программа должна работать не более чем за O(n*log log n) шагов.*/
+
+#include <iostream>
+using namespace std;
+
+void praim(bool B[], int N)
+{
+int i, P;
+int a = 1;
+
+for (P=2; P<=N; P++) B[P]=true;
+
+P=2;
+
+while (P*P<=N)
+{
+i=P*P;
+if (B[P])
+ while (i<=N)
+ {
+ B[i]=false;
+ i=i+P;
+ }
+P=P+1;
+}
+
+for (P=2; P<=N; P++) {
+if (B[P]== true) a = a * P;
+
+}
+
+cout << a;
+
+}
+
+int main(){
+ 
+ int N;
+
+ cout << "N#_"; 
+
+ cin >> N;
+
+ bool *B = new bool[N];
+
+ praim (B, N);
+
+return 0;
+
+}
+
